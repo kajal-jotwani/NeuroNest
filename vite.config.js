5c4f1d13f85dcdf42705886783924f1a5db93abb
kajal-jotwani <kajaljotwani06@gmail.com>
2024-11-10 04:20:08 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  base: './', // Replace 'NeuroNest' with your repository name
+   // Replace 'NeuroNest' with your repository name
 })
